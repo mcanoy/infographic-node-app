1b30ae56a87c279089734444de27cb157c332295
Kevin McAnoy <kmcanoy@redhat.com>
2016-08-09 12:49:11 -0700

    Create my own dev - mcanoy

--- DevJenkinsfile.groovy
+++ DevJenkinsfile.groovy
@@ -2,7 +2,7 @@ developmentPipeline{
     buildCommands = ['npm install -only=dev'] // Add this when we figure out , 'npm run-script validate']
     unitTestCommand = 'npm test'
     buildTool = 'node-0.10'
-    ocHost = 'holmes-playground.demo.innovation.redhat.com'
-    projectName = 'holmes-playground'
+    ocHost = 'mcanoy-playground.demo.innovation.redhat.com'
+    projectName = 'mcanoy-playground'
     appName = 'infographic-node-app'
 }
